1a5f35cf7790b9fd24e669c25d88ff4ef7f155be
Sebastian Ulloa <sebastian.ulloa@skyline.be>
2024-10-08 20:39:12 -0500

    feat: initial structure for user service

---
+++ app/main.py
@@ -0,0 +1,48 @@
+import os
+from fastapi import FastAPI, Request
+from fastapi.responses import JSONResponse
+from fastapi.exceptions import RequestValidationError
+
+from .errors.errors import ApiError
+from .routers import user, company, manager
+
+app = FastAPI()
+app.include_router(company.router)
+app.include_router(user.router)
+app.include_router(manager.router)
+version = "1.0"
+
+@app.get("/health")
+async def health():
+    return {"status": "OK"}
+
+@app.exception_handler(ApiError)
+async def api_error_exception_handler(request: Request, exc: ApiError):
+    return JSONResponse(
+        status_code=exc.code,
+        content={
+            "mssg": exc.description, 
+            "details": str(exc),
+            "version": version
+        },
+    )
+
+@app.exception_handler(RequestValidationError)
+async def validation_exception_handler(request: Request, exc: RequestValidationError):
+    errors = []
+    for error in exc.errors():
+        error_detail = {
+            "location": error["loc"],
+            "message": error["msg"],
+            "type": error["type"]
+        }
+        errors.append(error_detail)
+
+    return JSONResponse(
+        status_code=400,
+        content={
+            "message": "Validation Error",
+            "details": errors,
+            "version": version
+        },
+    )
---
+++ app/routers/company.py
@@ -0,0 +1,38 @@
+from fastapi import APIRouter, Depends, Header, Query, Path, HTTPException
+
+from ..schemas.user import CompanyCreate, CompanyUpdate, CompanyResponse
+from ..models.model import Company, save_user
+
+from sqlalchemy.orm import Session
+from ..session import get_db
+
+
+router = APIRouter(prefix="/company", tags=["Company"])
+
+@router.post("/", response_model=CompanyResponse, status_code=201)
+def create_company(company_schema: CompanyCreate, db: Session = Depends(get_db)):
+    if db.query(Company).filter(Company.username == company_schema.username).first():
+        raise HTTPException(status_code=400, detail="Email already registered")
+    
+    created_company = save_user(db, Company, company_schema)
+    return created_company
+
+@router.get("/{company_id}", response_model=CompanyResponse, status_code=200)
+def view_companies(
+    company_id: str = Path(..., description="Id of the offer", pattern="^[a-f\d]{8}(-[a-f\d]{4}){3}-[a-f\d]{12}$"),
+    db: Session = Depends(get_db)
+):
+    company = db.query(Company).filter(Company.id == company_id).first()
+    if not company:
+        raise HTTPException(status_code=404, detail="Company not found")
+    return company
+
+@router.put("/{company_id}", response_model=CompanyResponse)
+def update_company(company_id: str, company: CompanyUpdate):
+    # Implement update logic
+    pass
+
+@router.delete("/{company_id}")
+def delete_company(company_id: str):
+    # Implement delete logic
+    pass
